b0ec24d8bc133dbfd36810173021f15c1b24f489
Meet Jethwa <meetjethwa3@gmail.com>
2025-01-12 20:49:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,22 +16,35 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model = torch.hub.load("bryandlee/animegan2-pytorch:main", "generator", pretrained="face_paint_512_v2", device=device).eval()
 face2paint = torch.hub.load("bryandlee/animegan2-pytorch:main", "face2paint", device=device)
 
+# @app.post("/transform/")
+# async def transform_image(file: UploadFile = File(...)):
+#     contents = await file.read()
+#     image_format = "png"  # Set the image format
+
+#     # Open the input image
+#     im_in = Image.open(io.BytesIO(contents)).convert("RGB")
+#     im_out = face2paint(model, im_in, side_by_side=False)
+
+#     # Generate a unique filename to save the transformed image
+#     output_filename = f"{uuid.uuid4()}.{image_format}"
+#     output_path = f"/tmp/{output_filename}"  # Save in /tmp for Lambda-like environments
+
+#     # Save the transformed image
+#     im_out.save(output_path, format=image_format)
+
+#     # Return the file as a downloadable response
+#     return FileResponse(output_path, media_type=f"image/{image_format}", filename=output_filename)
+
 @app.post("/transform/")
 async def transform_image(file: UploadFile = File(...)):
     contents = await file.read()
     image_format = "png"  # Set the image format
 
-    # Open the input image
     im_in = Image.open(io.BytesIO(contents)).convert("RGB")
     im_out = face2paint(model, im_in, side_by_side=False)
 
-    # Generate a unique filename to save the transformed image
-    output_filename = f"{uuid.uuid4()}.{image_format}"
-    output_path = f"/tmp/{output_filename}"  # Save in /tmp for Lambda-like environments
+    buf = io.BytesIO()
+    im_out.save(buf, format=image_format)
+    buf.seek(0)
 
-    # Save the transformed image
-    im_out.save(output_path, format=image_format)
-
-    # Return the file as a downloadable response
-    return FileResponse(output_path, media_type=f"image/{image_format}", filename=output_filename)
-
+    return StreamingResponse(buf, media_type=f"image/{image_format}")
