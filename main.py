a8d2e58bb160229546358f8d37d3537faab492f2
Meet Jethwa <meetjethwa3@gmail.com>
2025-01-12 23:36:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,4 @@
-# from fastapi import FastAPI, File, UploadFile
-# from fastapi.responses import FileResponse
-# import torch
-# from PIL import Image
-# import io
-# import os
-# import uuid
-# from mangum import Mangum
 
-# app = FastAPI()
-
-# # Mangum handler for AWS Lambda
-# handler = Mangum(app)
-
-# # Assuming the model and device setup is similar to the provided snippet.
-# device = "cuda" if torch.cuda.is_available() else "cpu"
-# model = torch.hub.load("bryandlee/animegan2-pytorch:main", "generator", pretrained="face_paint_512_v2",device=device).eval()
-# face2paint = torch.hub.load("bryandlee/animegan2-pytorch:main", "face2paint", device=device)
-
-# @app.post("/transform/")
-# async def transform_image(file: UploadFile = File(...)):
-#     contents = await file.read()
-#     image_format = "png"  # Set the image format
-
-#     # Open the input image
-#     im_in = Image.open(io.BytesIO(contents)).convert("RGB")
-#     im_out = face2paint(model, im_in, side_by_side=False)
-
-#     # Generate a unique filename to save the transformed image
-#     output_filename = f"{uuid.uuid4()}.{image_format}"
-#     output_path = f"/tmp/{output_filename}"  # Save in /tmp for AWS Lambda
-
-#     # Save the transformed image
-#     im_out.save(output_path, format=image_format)
-
-#     # Return the file as a downloadable response
-#     return FileResponse(output_path, media_type=f"image/{image_format}", filename=output_filename)
-
-# # Mangum handler for AWS Lambda
-# handler = Mangum(app)
 
 from fastapi import FastAPI, File, UploadFile, HTTPException, Depends, Request
 from fastapi.responses import FileResponse, StreamingResponse
@@ -46,7 +7,6 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from PIL import Image
 import torch
-import redis
 import uuid
 import os
 import io
